767d6ed307cdf38529ca66d22082fbe480fa39fa
whycw010 <wahyucahyo2014@mail.ugm.ac.id>
2025-05-16 00:50:41 +0700

    fix app/FullNews/[id]/page.tsx

--- app/FullNews/[id]/page.tsx
+++ app/FullNews/[id]/page.tsx
@@ -1,38 +1,38 @@
 import { notFound } from "next/navigation";
 import Navbar from "@/Components/navbar";
-
 interface Params {
-  params: { id: string };
+    params: Promise<{ id: string }>;
 }
 
 const getNewsItem = async (id: string) => {
-const res = await fetch(`http://localhost:9999/api/news/${id}`, {
-cache: "no-store",
-      });
-  if (!res.ok) return null;
+    const res = await fetch(`http://localhost:9999/api/news/${id}`, {
+        cache: "no-store",
+    });
+    if (!res.ok) return null;
 
-  return res.json();
+    return res.json();
 };
 
 export default async function FullNewsPage({ params }: Params) {
-  const newsItem = await getNewsItem(params.id);
+    const resolvedParams = await params;
+    const newsItem = await getNewsItem(resolvedParams.id);
 
-  if (!newsItem) {
-    notFound(); // Jika data tidak ditemukan, tampilkan halaman 404
-  }
+    if (!newsItem) {
+        notFound(); // Jika data tidak ditemukan, tampilkan halaman 404
+    }
 
-  return (
-    <div>
-      <Navbar /> {/* Tambahkan Navbar di sini */}
-      <div className="max-w-5xl mx-auto px-4 py-10">
+    return (
+        <div>
+        <Navbar /> {/* Tambahkan Navbar di sini */}
+        <div className="max-w-5xl mx-auto px-4 py-10">
         <h1 className="text-3xl font-bold mb-4">{newsItem.title}</h1>
         <img
-          src={`${newsItem.imageBase64}`}
-          alt={newsItem.title}
-          className="w-full h-auto object-cover mb-4"
+        src={`${newsItem.imageBase64}`}
+        alt={newsItem.title}
+        className="w-full h-auto object-cover mb-4"
         />
         <p>{newsItem.body}</p>
-      </div>
-    </div>
-  );
+        </div>
+        </div>
+    );
 }
